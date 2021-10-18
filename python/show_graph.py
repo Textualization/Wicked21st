4cd4d64d10489ce30503b982f8405d0e779344ba
Pablo Duboue <pablo.duboue@gmail.com>
2021-10-18 06:42:34 -0700

    finished literate programming

--- python/show_graph.py
+++ python/show_graph.py
@@ -1,14 +1,18 @@
+import sys
+
 from wicked21st.graph import load_graph, Graph
 
 import graphviz
 
-graph_def = load_graph("map20210812.mm")
+graph_def = load_graph(sys.argv[1])
 
 for cat, catid in Graph.CATEGORIES:
     nodes = graph_def.node_classes[catid]
-    print("cat: '{}' (len: {})".format(cat, len(nodes)))
-    for node in nodes:
+    print("category: '{}' (size: {})".format(cat, len(nodes)))
+    for node in sorted(nodes, key=lambda x:graph_def.node_names[x]):
         print("\t" + graph_def.node_names[node])
+        print("\t\tLinks to: " + "; ".join(sorted(list(map(lambda x:graph_def.node_names[x], graph_def.outlinks[node])))))
 
-print(graph_def.show().source)
+if len(sys.argv) > 2:
+    print(graph_def.show().source)
 
