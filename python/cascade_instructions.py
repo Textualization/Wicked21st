58e09ed5afa8c840e57a761f6cd33992639ccc99
Pablo Duboue <pablo.duboue@gmail.com>
2021-11-18 00:50:59 -0800

    1-level cascading orderings

--- python/cascade_instructions.py
+++ python/cascade_instructions.py
@@ -1,9 +1,13 @@
 import sys
+import multiprocessing
+
 from collections import namedtuple
 
 from wicked21st.graph import load_graph, Graph
 
 import graphviz
+
+DEBUG=False
 
 if len(sys.argv) > 1:
     graph_file = sys.argv[1]
@@ -16,7 +20,6 @@ graph_def = load_graph(graph_file)
 node_list = list()
 
 cats = sorted(Graph.CATEGORIES, key=lambda x:x[0])
-
 
 for _, catid in cats:
     ncat = sorted(graph_def.node_classes[catid], key=lambda x:graph_def.ordering[x])
@@ -41,6 +44,8 @@ for catid in graph_def.node_classes:
             if code in node_to_code.values():
                 raise Error(graph_def.node_names[node]+ " " + str(node_to_code))
         node_to_code[node] = code
+        
+code_to_node = { c: n for n, c in node_to_code.items() }
 
 
 def cascade0(node, graph, visited=None):
@@ -57,7 +62,7 @@ def cascade0(node, graph, visited=None):
     if result:
         return result
     # recurse
-    for outlink in outlinks:
+    for outlink in graph_def.outlinks[node]:
         result = result + cascade0(outlink, graph, list(visited))
     
     return result
@@ -144,13 +149,31 @@ def pp_instructions(instructions, indent=''):
 def pp_node_instructions(instructions):
     for n, instr in instructions:
         if len(instr) == 1:
-            print("{}: {}".format(graph_def.node_names[n], " ^ ".join(instr[0])))
+            print("{} [{}]: {}".format(graph_def.node_names[n], node_to_code[n], " ^ ".join(instr[0])))
         else:
-            print("{}:".format(graph_def.node_names[n]))
+            print("{} [{}]:".format(graph_def.node_names[n], node_to_code[n]))
             for ins in sorted(instr, key=lambda x:len(x)):
                 print("  {}".format(" ^ ".join(ins)))
 
 def simplify(conds):
+    conds = sorted(conds, key=lambda x:len(x))
+    result = list()
+    while conds:
+        cond0 = conds[0]
+        result.append(cond0)
+        old = conds[1:]
+        conds = list()
+        for cond in old:
+            subsumed = True
+            for c in cond0:
+                if c not in cond:
+                    subsumed = False
+                    break
+            if not subsumed:
+                conds.append(cond)
+    return result
+
+def simplify_rec(conds):
     conds = sorted(conds, key=lambda x:len(x))
     if conds:
         cond0 = conds[0]
@@ -165,70 +188,139 @@ def simplify(conds):
             if not subsumed:
                 rest.append(cond)
             
-            conds = [ cond0 ] + simplify(rest)
+            conds = [ cond0 ] + simplify_rec(rest)
     return conds
+
+def process_state(t):
+    node, state, in_reach, node_to_idx = t
+    graph = 1 << node_to_idx[node]
+    for idx in range(len(in_reach)):
+        if 1 << idx & state:
+            graph += 1 << node_to_idx[in_reach[idx]]
+
+    reached = cascade0(node, graph)
+    return reached, graph
+
+def make_conds(t):
+    n, graphs, in_reach, node_to_idx = t
+    if DEBUG:
+        print("Node {}, graphs: {:,}".format(node_to_code[n], len(graphs)))
     
+    relevant = []
+    for other in in_reach:
+        if other == n:
+            continue
+        idx = node_to_idx[other]
+
+        yes = list()
+        no = list()
+
+        for g in graphs:
+            if 1 << idx & g:                   
+                yes.append(g - 1 << idx)
+            else:
+                no.append(g)
+        if len(yes) != len(no):
+            relevant.append(idx)
+        else:
+            if sorted(yes) != sorted(no):
+                relevant.append(idx)
+    conds = list()
+    covered = set()
+    for g in graphs:
+        this_instr = list()
+        for r in relevant:
+            name = node_to_code[node_list[r]]
+            if 1 << r & g:
+                this_instr.append(name)
+            #else:
+            #    this_instr.append('!' + name)
+        instr_str = " ^ ".join(this_instr)
+        if instr_str not in covered:
+            conds.append(this_instr)
+            covered.add(instr_str)
+
+    # simplify, find smaller set and remove the rest
+    if DEBUG:
+        print("Node {} conds: {:,}".format(node_to_code[n], len(conds)))
+    conds = simplify(conds)
+
+    if DEBUG:
+        print("Node {} DONE".format(node_to_code[n]))
+    return (n, conds)
+    
+
+pool = multiprocessing.Pool(20)
+
 
 def node_instructions(node):
 
-    in_reach = list(set(reachable(node)))
+    in_reach = set(reachable(node))
+    in_reach.discard(node)
+    in_reach = sorted(list(in_reach), key=lambda x:graph_def.ordering[x])
 
     node_cascade_cond = { n: list() for n in in_reach }
-
     this_node_to_idx = { n: idx for n, idx in enumerate(in_reach) }
 
-    for state in range(1 << len(in_reach)):
+    if False:
+        for state in range(1 << len(in_reach)):
 
-        graph = 0
-        for idx in range(len(in_reach)):
-            if 1 << idx & state:
-                graph += 1 << node_to_idx[in_reach[idx]]
+            graph = 1 << node_to_idx[node]
+            for idx in range(len(in_reach)):
+                if 1 << idx & state:
+                    graph += 1 << node_to_idx[in_reach[idx]]
 
-        reached = cascade1(node, graph)
-        for n in reached:
-            node_cascade_cond[n].append(graph)
-
+            reached = cascade1(node, graph)
+            for n in reached:
+                node_cascade_cond[n].append(graph)
+    else:
+        for reached, graph in pool.map(process_state, map(lambda x: (node, x, in_reach, node_to_idx), range(1 << len(in_reach)))):
+            for n in reached:
+                node_cascade_cond[n].append(graph)
+                
     # simplify conditions
-    result = list()
-    for n, graphs in node_cascade_cond.items():
-        relevant = []
-        for other in in_reach:
-            if other == n:
-                continue
-            idx = node_to_idx[other]
+    if False:
+        for n, graphs in node_cascade_cond.items():
+            relevant = []
+            for other in in_reach:
+                if other == n:
+                    continue
+                idx = node_to_idx[other]
 
-            yes = list()
-            no = list()
+                yes = list()
+                no = list()
 
-            for g in graphs:
-                if 1 << idx & g:                   
-                    yes.append(g - 1 << idx)
-                else:
-                    no.append(g)
-            if len(yes) != len(no):
-                relevant.append(idx)
-            else:
-                if sorted(yes) != sorted(no):
+                for g in graphs:
+                    if 1 << idx & g:                   
+                        yes.append(g - 1 << idx)
+                    else:
+                        no.append(g)
+                if len(yes) != len(no):
                     relevant.append(idx)
-        conds = list()
-        covered = set()
-        for g in graphs:
-            this_instr = list()
-            for r in relevant:
-                name = node_to_code[node_list[r]]
-                if 1 << r & g:
-                    this_instr.append(name)
-                #else:
-                #    this_instr.append('!' + name)
-            instr_str = " ^ ".join(this_instr)
-            if instr_str not in covered:
-                conds.append(this_instr)
-                covered.add(instr_str)
+                else:
+                    if sorted(yes) != sorted(no):
+                        relevant.append(idx)
+            conds = list()
+            covered = set()
+            for g in graphs:
+                this_instr = list()
+                for r in relevant:
+                    name = node_to_code[node_list[r]]
+                    if 1 << r & g:
+                        this_instr.append(name)
+                    #else:
+                    #    this_instr.append('!' + name)
+                instr_str = " ^ ".join(this_instr)
+                if instr_str not in covered:
+                    conds.append(this_instr)
+                    covered.add(instr_str)
 
-        # simplify, find smaller set and remove the rest
-        conds = simplify(conds)
-            
-        result.append( (n, conds) )
+            # simplify, find smaller set and remove the rest
+            conds = simplify(conds)
+
+            result.append( (n, conds) )
+    else:
+        result = pool.map(make_conds, map(lambda x: (x[0], x[1], in_reach, node_to_idx), node_cascade_cond.items()))
     return result
                 
 
@@ -250,13 +342,20 @@ if False:
 else:
     for node in node_list:
         in_reach = list(set([node] + reachable(node)))
-        print('***', graph_def.node_names[node], len(in_reach), '***')
-        pp_node_instructions(node_instructions(node))
-
-
-#    break
-
-#    this_node_to_idx = { n: idx for enumerate(in_reach) }
-
-#    all_graphs = list()
-#    for 
+        print('*** {} [{}]: {} ***'.format(graph_def.node_names[node], node_to_code[node], len(in_reach)))
+        instr = node_instructions(node)
+        recorded = set()
+        for _, inss in instr:
+            for ins in inss:
+                recorded.update(set(ins))
+        recorded = set([ code_to_node[c] for c in recorded ])
+        r = list()
+        for n in node_list:
+            if n in recorded:
+                r.append(node_to_code[n])
+        print("    record ({}): {}".format(len(r), ", ".join(r)))
+        if len(in_reach) < 30:
+            pp_node_instructions(instr)
+        else:
+            print("  SKIPPED")
+        print()
---
+++ python/graph_to_cascades.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Textualization Software Ltd.
+# Distributed under the terms of the Apache Software License 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+import random
+import sys
+import multiprocessing
+
+from collections import namedtuple
+
+from wicked21st.graph import load_graph, Graph
+
+import graphviz
+
+DEBUG=False
+rand = random.Random(42) 
+
+if len(sys.argv) > 1:
+    graph_file = sys.argv[1]
+else:
+    import config
+    graph_file = config.GRAPH
+
+graph_def = load_graph(graph_file)
+
+
+
+node_list = list()
+
+cats = sorted(Graph.CATEGORIES, key=lambda x:x[0])
+
+for _, catid in cats:
+    ncat = sorted(graph_def.node_classes[catid], key=lambda x:graph_def.ordering[x])
+    node_list = node_list + ncat
+
+node_to_idx = { n: idx for idx, n in enumerate(node_list) }
+num_nodes = len(node_list)
+
+node_to_code = dict()
+for catid in graph_def.node_classes:
+    nodes = graph_def.node_classes[catid]
+    
+    for node in sorted(nodes, key=lambda x:graph_def.ordering[x]):
+        name = graph_def.node_names[node].upper()
+        if name[0] == '*':
+            name
+        if name.startswith("LACK OF"):
+            name = name[len("LACK OF "):]
+        code = name[:3]
+        if code in node_to_code.values():
+            code = name.split(" ")[1][:3]
+            if code in node_to_code.values():
+                raise Error(graph_def.node_names[node]+ " " + str(node_to_code))
+        node_to_code[node] = code
+        
+code_to_node = { c: n for n, c in node_to_code.items() }
+
+def reachable(node, path):
+    if node in path:
+        return list()
+    path = path + [ node ]
+    result = [ (node, path) ]
+    for outlink in sorted(graph_def.outlinks[node]):
+        result = result + reachable(outlink, path)
+    
+    return result
+
+with open("cascading.tsv", "w") as tsv:
+    for cnode in node_list:
+        in_reach = reachable(cnode, [])
+        shortest = dict()
+        for node, path in in_reach:
+            if node in shortest:
+                existing = shortest[node]
+                if len(existing[0]) > len(path):
+                    # replace
+                    shortest[node] = [ path ]
+                elif len(existing[0]) == len(path):
+                    shortest[node].append(path)
+            else:
+                shortest[node] = [ path ]
+
+        # sort by distance
+        by_distance = dict() # int -> list of (node, (list previous nodes) )
+        for node, paths in shortest.items():
+            dist = len(paths[0])
+            if dist not in by_distance:
+                by_distance[dist] = list()
+            prev = [ path[-1] for path in paths ]
+            by_distance[dist].append( (node, prev) )
+
+        # randomize
+        for dist in by_distance:
+            rand.shuffle(by_distance[dist])
+
+        # print
+        dist_buckts = list()
+        for dist in sorted(by_distance.keys()):
+            if dist > 2:
+                dist_buckts.append(list(map(lambda x:x[0], by_distance[dist])))
+        flatten = [ n for bckt in dist_buckts for n in bckt ]
+        print("{} ({}): {}".format(graph_def.node_names[cnode], node_to_code[cnode],
+                                   "; ".join(map(lambda bckt: ", ".join(map(lambda x:node_to_code[x], bckt)), dist_buckts))))
+        tsv.write("{}\t{}\n".format(cnode, "\t".join(flatten)))
+
+
+
