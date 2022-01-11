6fd5fa843e9109b156875fe352561fe312930382
Pablo Duboue <pablo.duboue@gmail.com>
2022-01-11 03:48:16 -0800

    ready for next run

--- python/config.py
+++ python/config.py
@@ -1,8 +1,8 @@
 SEED = 42
 NUM_RUNS = 1000
-GRAPH = "map20211122-small.json"
+GRAPH = "maps/map20211122.json"
 VERBOSE = True
-IN_CRISISF = [ 'Social Inequity',
+IN_CRISIS = [ 'Social Inequity',
               'Lack of Affordable Housing',
               'Lack of Community Networks',
               'Systemic Corruption',
@@ -12,14 +12,6 @@ IN_CRISISF = [ 'Social Inequity',
               'Fossil Fuel Dependency',
               'Market Externalities',
              ]
-IN_CRISIS = [ 'Social Inequity',
-              'Insufficient Housing & Energy',
-              'Systemic Corruption',
-              'Monopoly',
-              'Unsustainable Harvesting',
-              'Pollution',
-              'Market Externalities',
-             ]
-GAME_LENGTH = 12
+GAME_LENGTH = 8
 CRISIS_CHECK = 6
 CRISIS_RISING = 4
