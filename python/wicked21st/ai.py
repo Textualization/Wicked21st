9d0677b4ff7541a02d070a070dca6b70f9029b34
Pablo Duboue <pablo.duboue@gmail.com>
2022-01-22 04:18:53 -0800

    cleaning and reformatting

--- python/wicked21st/ai.py
+++ python/wicked21st/ai.py
@@ -26,15 +26,18 @@ class GreedyPlayer(Player):
         super().__init__(name, ordering)
         self.rnd = random.Random(ordering * 7)
 
+    def debug(self, *args):
+        """Debug print-outs, if flag is activated"""
+        if GreedyPlayer.DEBUG:
+            print(*args)
+
     def pick(
         self,
         decision: int,
         decisions: list,
-        # state: PlayerState
-        # game: GameState
         rand: random.Random,
-        state: object = None,
-        game: object = None,
+        state: object = None,  # state: PlayerState
+        game: object = None,  # game: GameState
         context: dict = None,
     ):
         """Choose a decision among many"""
@@ -51,126 +54,56 @@ class GreedyPlayer(Player):
         self,
         decision: int,
         decisions: list,
-        # state: PlayerState
-        # game: GameState
         rand: random.Random,
-        state: object = None,
-        game: object = None,
+        state: object = None,  # state: PlayerState
+        game: object = None,  # game: GameState
         context: dict = None,
     ):
-
+        """Internal pick, this is the method that gets the job done."""
         if state is None:
             print("ai player pick: Missing state")
             return rand.choice(decisions)
 
         if decision == Player.INIT_ROLE:
-            game_def = context["game_def"]
-            if state.extra is None:
-                state.extra = GreedyPlayerState(self.rnd, state, game, game_def)
-            # pick role that cover most of the top categories
-            scores = sorted(
-                [
-                    (role_name, self.score_class(role_name, state, game, game_def))
-                    for role_name in decisions
-                ],
-                key=lambda x: x[1],
-            )
-            return scores[-1][0]
-
-        # now get to work
+            return self._pick_class(decisions, rand, state, game, context)
         if decision == Player.PILE_DRAW:
-            if (
-                "drawn" not in state.extra.phase_mem
-                or state.extra.phase_mem["drawn"] == 2
-            ):
-                game_def = context["game_def"]
-                actions, extra = self.analyze(self.rnd, state, game, game_def)
-                state.extra.phase_mem = {"drawn": 0, "actions": actions}  # reset
-                state.extra.phase_mem.update(extra)
-            else:
-                actions = state.extra.phase_mem["actions"]
-
-            chosen = actions[Player.PILE_DRAW][state.extra.phase_mem["drawn"]]
-            state.extra.phase_mem["drawn"] += 1
-            return chosen
+            return self._pick_pile(decisions, rand, state, game, context)
 
         actions = state.extra.phase_mem["actions"]
+
         if decision == Player.START_PROJECT:
             project = actions[Player.START_PROJECT]
             if project:
                 return project["type"]
             return None
-        elif decision == Player.START_PROJECT_FIX_CAT:
+        if decision == Player.START_PROJECT_FIX_CAT:
             project = actions[Player.START_PROJECT]
             return project["category"]
-        elif decision == Player.START_PROJECT_FIX_NODE:
+        if decision == Player.START_PROJECT_FIX_NODE:
             project = actions[Player.START_PROJECT]
             return project["node"]
 
         if "card_analysis" not in state.extra.phase_mem:
             actions = self.analyze_cards(self.rnd, state, game, actions)
-            if GreedyPlayer.DEBUG:
-                print("cards analyzed:", actions)
+            self.debug("cards analyzed:", actions)
             state.extra.phase_mem["actions"] = actions
             state.extra.phase_mem["card_analysis"] = True
 
         if decision == Player.PLAY_CARD:
-            cards = actions[Player.PLAY_CARD]
-            cardno = state.extra.phase_mem.get("play_card", 0)
-            state.extra.phase_mem["play_card"] = cardno + 1
-            if cards:
-                if cardno >= len(cards):
-                    return None
-                for dec in decisions:
-                    card, suit, _, project_name = dec
-                    if (
-                        cards[cardno][0] == card
-                        and cards[cardno][1] == suit
-                        and cards[cardno][2] == project_name
-                    ):
-                        return dec
-                print(
-                    "Card not found: cards={}, cardno={}, decisions={}".format(
-                        cards, cardno, decisions
-                    )
-                )
-            return None
-        elif decision == Player.CONSULTANT:
+            return self._pick_card(decisions, rand, state, game, context, actions)
+        if decision == Player.CONSULTANT:
             cards = actions[Player.PLAY_CARD]
             return cards[state.extra.phase_mem["play_card"] - 1][3]
-        elif decision == Player.START_RESEARCH:
+        if decision == Player.START_RESEARCH:
             return actions[Player.START_RESEARCH]
-        elif decision == Player.CARD_FOR_RESEARCH:
-            cards = actions[Player.CARD_FOR_RESEARCH]
-            cardno = state.extra.phase_mem.get("research_card", 0)
-            state.extra.phase_mem["research_card"] = cardno + 1
-
-            if cards:
-                if cardno >= len(cards):
-                    return None
-                for dec in decisions:
-                    tech_name, card, _, suit = dec
-                    if (
-                        cards[cardno][0] == card
-                        and cards[cardno][1] == suit
-                        and cards[cardno][2] == tech_name
-                    ):
-                        return dec
-                print(
-                    "card for research not found: cards={}, cardno={}, decisions={}".format(
-                        cards, cardno, decisions
-                    )
-                )
-            return None
-        elif decision == Player.FUND_RESEARCH:
-            to_fund = actions[Player.FUND_RESEARCH]
-            fundno = state.extra.phase_mem.get("research_fund", 0)
-            state.extra.phase_mem["research_fund"] = fundno + 1
-            if to_fund:
-                if fundno >= len(to_fund):
-                    return None
-                return to_fund[fundno]
-            return None
+        if decision == Player.CARD_FOR_RESEARCH:
+            return self._pick_card_research(
+                decisions, rand, state, game, context, actions
+            )
+        if decision == Player.FUND_RESEARCH:
+            return self._pick_fund_research(
+                decisions, rand, state, game, context, actions
+            )
 
         print(
             "something went wrong: decision={}, decisions={}".format(
@@ -178,6 +111,131 @@ class GreedyPlayer(Player):
             )
         )
         return rand.choice(decisions)
+
+    def _pick_class(
+        self,
+        decisions: list,
+        rand: random.Random,
+        state: object = None,  # state: PlayerState
+        game: object = None,  # game: GameState
+        context: dict = None,
+    ):
+        """Pick a class"""
+        game_def = context["game_def"]
+        if state.extra is None:
+            state.extra = GreedyPlayerState(self.rnd, state, game, game_def)
+        # pick role that cover most of the top categories
+        scores = sorted(
+            [
+                (role_name, self.score_class(role_name, state, game, game_def))
+                for role_name in decisions
+            ],
+            key=lambda x: x[1],
+        )
+        return scores[-1][0]
+
+    def _pick_pile(
+        self,
+        decisions: list,
+        rand: random.Random,
+        state: object = None,  # state: PlayerState
+        game: object = None,  # game: GameState
+        context: dict = None,
+    ):
+        """Pick a pile to draw from"""
+        if "drawn" not in state.extra.phase_mem or state.extra.phase_mem["drawn"] == 2:
+            game_def = context["game_def"]
+            actions, extra = self.analyze(self.rnd, state, game, game_def)
+            state.extra.phase_mem = {"drawn": 0, "actions": actions}  # reset
+            state.extra.phase_mem.update(extra)
+        else:
+            actions = state.extra.phase_mem["actions"]
+
+        chosen = actions[Player.PILE_DRAW][state.extra.phase_mem["drawn"]]
+        state.extra.phase_mem["drawn"] += 1
+        return chosen
+
+    def _pick_card(
+        self,
+        decisions: list,
+        rand: random.Random,
+        state: object = None,  # state: PlayerState
+        game: object = None,  # game: GameState
+        context: dict = None,
+        actions: dict = None,
+    ):
+        """Pick a card to play"""
+        cards = actions[Player.PLAY_CARD]
+        cardno = state.extra.phase_mem.get("play_card", 0)
+        state.extra.phase_mem["play_card"] = cardno + 1
+        if cards:
+            if cardno >= len(cards):
+                return None
+            for dec in decisions:
+                card, suit, _, project_name = dec
+                if (
+                    cards[cardno][0] == card
+                    and cards[cardno][1] == suit
+                    and cards[cardno][2] == project_name
+                ):
+                    return dec
+            print(
+                "Card not found: cards={}, cardno={}, decisions={}".format(
+                    cards, cardno, decisions
+                )
+            )
+        return None
+
+    def _pick_card_research(
+        self,
+        decisions: list,
+        rand: random.Random,
+        state: object = None,  # state: PlayerState
+        game: object = None,  # game: GameState
+        context: dict = None,
+        actions: dict = None,
+    ):
+        """Pick a card for research"""
+        cards = actions[Player.CARD_FOR_RESEARCH]
+        cardno = state.extra.phase_mem.get("research_card", 0)
+        state.extra.phase_mem["research_card"] = cardno + 1
+
+        if cards:
+            if cardno >= len(cards):
+                return None
+            for dec in decisions:
+                tech_name, card, _, suit = dec
+                if (
+                    cards[cardno][0] == card
+                    and cards[cardno][1] == suit
+                    and cards[cardno][2] == tech_name
+                ):
+                    return dec
+            print(
+                "card for research not found: cards={}, cardno={}, decisions={}".format(
+                    cards, cardno, decisions
+                )
+            )
+        return None
+
+    def _pick_fund_research(
+        self,
+        decisions: list,
+        rand: random.Random,
+        state: object = None,  # state: PlayerState
+        game: object = None,  # game: GameState
+        context: dict = None,
+        actions: dict = None,
+    ):
+        """Pick funds for research"""
+        to_fund = actions[Player.FUND_RESEARCH]
+        fundno = state.extra.phase_mem.get("research_fund", 0)
+        state.extra.phase_mem["research_fund"] = fundno + 1
+        if to_fund:
+            if fundno >= len(to_fund):
+                return None
+            return to_fund[fundno]
+        return None
 
     def score_class(self, class_name, state, game, game_def):
         cat_importance = state.extra.cat_importance
@@ -195,6 +253,7 @@ class GreedyPlayer(Player):
         return score
 
     def analyze(self, rnd, state, game, game_def):
+        """The core of the AI, analyze the state of the board and decide next actions"""
 
         # choose research
         researching = list(game.tech.techs_for_status(TechTreeState.IN_PROGRESS))
@@ -237,8 +296,7 @@ class GreedyPlayer(Player):
                 if target_tech:
                     break
 
-        if GreedyPlayer.DEBUG:
-            print("target_tech: ", target_tech.name if target_tech else None)
+        self.debug("target_tech: ", target_tech.name if target_tech else None)
 
         # else: do not do any research
 
@@ -259,21 +317,19 @@ class GreedyPlayer(Player):
                 elif idx < len(order) * 0.666:
                     crisis_top_66perc.append(node)
 
-        if GreedyPlayer.DEBUG:
-            print(
-                "crisis_top_16perc={}, crisis_top_33perc={}, crisis_top_66perc={}".format(
-                    len(crisis_top_16perc),
-                    len(crisis_top_33perc),
-                    len(crisis_top_66perc),
-                )
+        self.debug(
+            "crisis_top_16perc={}, crisis_top_33perc={}, crisis_top_66perc={}".format(
+                len(crisis_top_16perc),
+                len(crisis_top_33perc),
+                len(crisis_top_66perc),
             )
+        )
 
         # choose project
         ongoing = game.projects.projects_for_status(ProjectState.IN_PROGRESS)
 
         if crisis_top_16perc:
-            if GreedyPlayer.DEBUG:
-                print("crisis_top_16perc")
+            self.debug("crisis_top_16perc")
 
             # there are projects for them?
             projects = list()
@@ -432,8 +488,7 @@ class GreedyPlayer(Player):
                         Player.START_RESEARCH: None,
                     }, {"project": {"name": project.name, "priority": "top"}}
         elif crisis_top_33perc:
-            if GreedyPlayer.DEBUG:
-                print("crisis_top_33perc")
+            self.debug("crisis_top_33perc")
             if state.projects:  # do I have a project?
                 # finish it
                 needed = list(
@@ -514,8 +569,7 @@ class GreedyPlayer(Player):
                     else None,
                 }, {"project": {"name": project.name, "priority": "medium"}}
         elif crisis_top_66perc:
-            if GreedyPlayer.DEBUG:
-                print("crisis_top_66perc")
+            self.debug("crisis_top_66perc")
             # same as before, but focus on research for top 16%
             draw = [None, None]
             if target_tech.suit in [self.player_class.suit_a, self.player_class.suit_b]:
@@ -597,8 +651,7 @@ class GreedyPlayer(Player):
                     else None,
                 }, {"project": {"name": project.name, "priority": "low"}}
         else:
-            if GreedyPlayer.DEBUG:
-                print("crisis: other")
+            self.debug("crisis: other")
             # research and hoard cards for top 16%
             draw = [None, None]
             if target_tech and target_tech.suit in [
@@ -636,7 +689,8 @@ class GreedyPlayer(Player):
         }, {}
 
     def analyze_cards(self, rnd, state, game, actions):
-        # check the cards drawn and use them to fund project and tech using the priorities
+        """check the cards drawn and use them to fund project and tech using the priorities"""
+
         play_card = None
         in_hand = list(state.cards)
         money = state.resources["$"]
@@ -744,12 +798,16 @@ class GreedyPlayerState:
 
             self.phase_mem = {}
 
-            if GreedyPlayer.DEBUG:
-                print("My top tier:")
-                for idx, node in enumerate(self.order):
-                    if idx >= len(self.order) * GreedyPlayer.TOP_TIER:
-                        break
-                    print("\t" + game_def.graph.node_names[node])
+            self.debug("My top tier:")
+            for idx, node in enumerate(self.order):
+                if idx >= len(self.order) * GreedyPlayer.TOP_TIER:
+                    break
+                self.debug("\t" + game_def.graph.node_names[node])
+
+    def debug(self, *args):
+        """Debug print-outs, if flag is activated"""
+        if GreedyPlayer.DEBUG:
+            print(*args)
 
     def to_json(self):
         return {
